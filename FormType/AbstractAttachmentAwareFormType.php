57dde67d22ce1367199d2f4abe59085ebb4be5f9
VS <hawkeye3004@gmail.com>
2020-09-20 16:48:56 +0300

    Added minor fix

--- FormType/AbstractAttachmentAwareFormType.php
+++ FormType/AbstractAttachmentAwareFormType.php
@@ -18,8 +18,8 @@ abstract class AbstractAttachmentAwareFormType extends AbstractType
     public const FORM_CHILD_RECIPIENT   = 'recipient';
     public const FORM_CHILD_SUBJECT     = 'subject';
     public const FORM_CHILD_BODY        = 'body';
-    public const FORM_CHILD_ATTACHMENTS = 'recipient';
-    public const FORM_CHILD_SAVE        =  'save';
+    public const FORM_CHILD_ATTACHMENTS = 'attachments';
+    public const FORM_CHILD_SAVE        = 'save';
 
     abstract protected function innerBuild(FormBuilderInterface $builder, array $options): void;
 
@@ -32,17 +32,23 @@ abstract class AbstractAttachmentAwareFormType extends AbstractType
         $this->innerBuild($builder, $options);
 
         $builder
-            ->add(self::FORM_CHILD_ATTACHMENTS, FileType::class, [
-                'label' => 'attachment',
-                'multiple' => true,
-                'attr'     => [
-                    'accept' => 'image/*',
-                    'multiple' => 'multiple'
-                ],
-                'required' => false,
-                'translation_domain' => 'FOSMessageBundle',
-            ])
-            ->add(self::FORM_CHILD_SAVE, SubmitType::class,
+            ->add(
+                self::FORM_CHILD_ATTACHMENTS,
+                FileType::class,
+                [
+                    'label' => 'attachment',
+                    'multiple' => true,
+                    'attr' => [
+                        'accept' => 'image/*',
+                        'multiple' => 'multiple'
+                    ],
+                    'required' => false,
+                    'translation_domain' => 'FOSMessageBundle',
+                ]
+            )
+            ->add(
+                self::FORM_CHILD_SAVE,
+                SubmitType::class,
                 [
                     'label' => 'Save',
                     'translation_domain' => 'FOSMessageBundle',
