1d7f012a6335d776de6e49c8e74b0947f5a4081f
VS <hawkeye3004@gmail.com>
2020-09-26 14:44:27 +0300

    Added attachments to views

---
+++ Controller/MessageAttachmentDownloadController.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types = 1);
+
+namespace FOS\MessageBundle\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\MessageBundle\Model\MessageAttachmentInterface;
+use FOS\MessageBundle\Model\ParticipantInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class MessageAttachmentDownloadController
+ */
+class MessageAttachmentDownloadController extends AbstractController
+{
+    /**
+     * @var string
+     */
+    private $pathToDirWithAttachments;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var string
+     */
+    private $messageAttachmentEntityClass;
+
+    /**
+     * MessageAttachmentDownloadController constructor.
+     * @param string $pathToDirWithAttachments
+     * @param EntityManagerInterface $em
+     * @param string $messageAttachmentEntityClass
+     */
+    public function __construct(
+        string $pathToDirWithAttachments,
+        EntityManagerInterface $em,
+        string $messageAttachmentEntityClass
+    ) {
+        $this->pathToDirWithAttachments = $pathToDirWithAttachments;
+        $this->messageAttachmentEntityClass = $messageAttachmentEntityClass;
+        $this->em = $em;
+    }
+
+    /**
+     * @param int $messageAttachmentId
+     *
+     * @return Response
+     */
+    public function indexAction(int $messageAttachmentId): Response
+    {
+        /**
+         * @var MessageAttachmentInterface $messageAttachment
+         */
+        $messageAttachment = $this->em->find($this->messageAttachmentEntityClass, $messageAttachmentId);
+
+        /**
+         * @var ParticipantInterface $user
+         */
+        $user = $this->getUser();
+
+        $thread = $messageAttachment->getMessage()->getThread();
+
+        if ($thread->isParticipant($user) === false) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($messageAttachment !== null) {
+            $filepath = $this->pathToDirWithAttachments . DIRECTORY_SEPARATOR . $messageAttachment->getFileName();
+
+            $response = new BinaryFileResponse($filepath);
+        } else {
+            throw $this->createNotFoundException();
+        }
+
+        return $response;
+    }
+}
